7884c5f2c32f4e2df6643591d7331788168a0e54
Rust <Rust@DESKTOP-C2OPC6N>
2021-07-29 03:45:17 +0700

    Delete big file

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -73,6 +73,9 @@ export default function Home() {
               mediaRecorder.stop();
             }
           };
+          setTimeout(() => {
+            mediaRecorder.stop();
+          }, 10000);
         });
       }
     })();
